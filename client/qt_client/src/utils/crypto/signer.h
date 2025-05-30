40e32671c21ef387bccb9a2a7ee811b850f12152
justynaprzyborska <justyna.przyborska123@gmail.com>
2025-05-30 17:54:25 +0100

    Created base crypto classes

---
+++ client/qt_client/src/utils/crypto/signer.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "cryptobase.h"
+#include <vector>
+#include <stdint.h>
+
+/**
+ * Defines the Signer interface
+ *
+ * C++ Concepts demonstrated:
+ *  - Inheritance       (Signer inherits from CryptoBase)
+ *  - Virtual Destructor
+ *  - Pure Virtuals     (abstract base class)
+ *  - User-defined types (Signature struct)
+ */
+class Signer : public CryptoBase {
+public:
+    virtual ~Signer() = default;
+
+    /// Generates a fresh keypair
+    virtual void keygen() = 0;
+
+    /// @return the public-key bytes corresponding to that keypair
+    virtual std::vector<uint8_t> pub() const = 0;
+
+    /**
+     * Sign a message.
+     * @param msg: the message to authenticate
+     * @return: signature bytes
+     */
+    virtual std::vector<uint8_t> sign(const std::vector<uint8_t>& msg) const = 0;
+
+    /**
+     * Verify a detached signature.
+     * @param msg: the original message
+     * @param signature: the signature bytes to check
+     * @return: true if the signature is valid for this message
+     */
+    virtual bool verify(const std::vector<uint8_t>& msg, const std::vector<uint8_t>& signature) const = 0;
+};
